6a5680b503c92bc0086957db9a8dcb11c3036852
Pedro Petrilli <ppetrilli96@gmail.com>
2024-04-15 16:26:15 -0300

    test: teste para dois dias acima da média

--- src/test/java/TemperaturaTest.java
+++ src/test/java/TemperaturaTest.java
@@ -11,10 +11,18 @@ public class TemperaturaTest {
     void DeveRetornar1SeUmDiaForAcimaDaMedia(){
         assertEquals(1, Temperatura.diasAcimaDaMedia(new int[] { 2, 2, 2, 2, 2, 2, 3 }));
     }
+
+    @Test
+    @DisplayName("Deve retornar 2 se dois dias forem acima da média")
+    void deveRetornar2SeDoisDiasForemAcimaDaMedia(){
+        assertEquals(2, Temperatura.diasAcimaDaMedia(new int[] { 2, 2, 2, 2, 2, 3, 3 }));
+    }
     @Test
     @DisplayName("Deve retornar 3 se tres dias forem acima da média")
     void deveRetornar3SeTresDiasForemAcimaDaMedia(){
         assertEquals(3, Temperatura.diasAcimaDaMedia(new int[] { 2, 2, 3, 4, 5, 6, 1 }));
     }
+
+
 }
 
